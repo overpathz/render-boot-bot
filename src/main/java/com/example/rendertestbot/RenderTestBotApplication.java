8bcc2433aa19a3fc6cf89e4d0795f490ac4d9fe9
overpathz <overpathz@gmail.com>
2024-04-20 12:32:33 +0300

    init

---
+++ src/main/java/com/example/rendertestbot/RenderTestBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.rendertestbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderTestBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenderTestBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rendertestbot/RenderTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rendertestbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderTestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
